c148d73c6c8ef12dd48d901cae137f3fbda912ea
charan-sun <122475385+charan-sun@users.noreply.github.com>
2023-04-04 22:40:06 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int a;
+  scanf("%d",&a);
+
+
+
+  if (a>0)
+  {
+      printf("%d is positive integer",a);
+  }
+
+  if (a<0)
+  {
+      printf("it is negative integer");
+  }
+
+
+   else
+   {
+       printf("it is zero");
+   }
+
+}
